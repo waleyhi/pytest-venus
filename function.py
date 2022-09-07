b840e9b6723a41cb31e059e53b5efd660d6107c8
waleyhi <1107977477@qq.com>
2022-09-07 16:39:22 +0800

    新增基本功能函数和venus-messager测试用例

--- function.py
+++ function.py
@@ -1,6 +1,12 @@
 import psutil
-#检查进程是否存在函数,返回1表示存在，0表示不存在
+import os
+#获取进程是否存在函数,返回1表示存在，0表示不存在
 def process_check(process_name):
+    '''
+    用于检查进程是否存在
+    参数填写进程名称
+    返回1表示该进程存在，0表示进程不存在
+    '''
     for i in psutil.pids():
         if psutil.Process(i).name()==f"{process_name}":
             print(f"{process_name}进程正在运行,进程号为:%s" % i)
@@ -12,7 +18,46 @@ def process_check(process_name):
 
 #获取进程创建的时间
 def process_alive(process_name,process_cmd):
+    '''
+    用于获取进程创建的时间
+    :param process_name: 进程名称
+    :param process_cmd: 进程启动带的第一个参数
+    :return:
+    '''
     process_pid = [pid for pid in psutil.pids() if psutil.Process(pid).name() == f"{process_name}" and psutil.Process(pid).cmdline()[1] == f"{process_cmd}"]
     process_create_time=psutil.Process(process_pid[0]).create_time()
     return process_create_time
+#venus-message获取各种类型消息id用于自动化测试,如果没有查询到消息id，则返回0
+def venus_messager_id_get(list_type):
+    '''
+    用于获取消息id
+    可选参数为list，list-fail或者list-blocked
+    '''
+    messager_msg_info = os.popen(f"/root/venus-messager msg {list_type} --verbose| grep '^baf'").readlines()
+    if len(messager_msg_info)==0:
+        print ("无可供测试的消息")
+        return 0
+    else:
+        messager_msg_id=messager_msg_info[0].split()[0]
+        return messager_msg_id
+def venus_messager_address_get():
+    '''
+    用于获取venus-messager中address地址
+    :return:
+    '''
+    messager_address_info = os.popen(f"/root/venus-messager address list").readlines()
+    if len(messager_address_info)==0:
+        print("无可供测试的地址信息")
+        return 0
+    else:
+        messager_address_addr=messager_address_info[3].split('"')[3]
+        return messager_address_addr
+def venus_messager_address_state(address):
+    messager_address_info = os.popen(f"/root/venus-messager address search {address}").readlines()
+    if len(messager_address_info) > 2:
+        messager_address_state = messager_address_info[6].split()[1]
+        return messager_address_state
+    else:
+        print("无法找到对应钱包地址信息，请检查钱包地址信息是否正常")
+        return 0
 
--- test_venus-messager.py
+++ test_venus-messager.py
@@ -1,27 +1,160 @@
 import os
 import allure
 import time
-import psutil
 import pytest
 import function
 @allure.epic("venus-messager测试")
 @allure.feature("venus-messager主程序测试")
 class Test_venus_messager_status():
     @allure.story("测试venus-messager程序是否能启动")
+    @pytest.mark.run(order=1)
     def test_venus_messager_start(self):
-        a=function.process_check(venus-messager)
+        a=function.process_check('venus-messager')
         assert a==1,"venus-messager 进程不存在"
     @allure.story("测试venus-messager程序是否能稳定运行3分钟不崩溃")
+    @pytest.mark.run(order=1)
     def test_venus_messager_alive(self):
         #time.sleep(180)
-        a=function.process_alive(venus-messager,run)
+        a=function.process_alive('venus_messager','run')
         b=time.time()
         c=b-a
         print ("venus-messager 进程已稳定运行%f秒" % c)
         assert c > 180,"venus-messager 运行时间不足3分钟"
 
+@allure.epic("venus-messager测试")
+@allure.feature("venus-messager msg功能模块测试")
+class Test_venus_messager_msg():
+    @allure.story("测试venus-messager msg list查看消息是否正常")
+    @pytest.mark.run(order=2)
+    def test_venus_messager_msg_list(self):
+        try:
+            messager_msg_list = os.popen("/root/venus-messager msg list").readlines()
+            print ("messager msg list执行结果为：",messager_msg_list)
+            a=1
+        except Exception as e:
+            print("命令报错,错误信息为：",e)
+            a=0
+        assert a==1,"messager msg list 执行报错"
+    @allure.story("测试venus-messager msg list-fail查看失败消息是否正常")
+    @pytest.mark.run(order=2)
+    def test_venus_messager_msg_list_fail(self):
+        try:
+            messager_msg_list_fail = os.popen("/root/venus-messager msg list-fail").readlines()
+            print ("messager msg list-fail执行结果为：",messager_msg_list_fail)
+            a=1
+        except Exception as e:
+            print("命令报错,错误信息为：",e)
+            a=0
+        assert a==1,"messager msg list-fail 执行报错"
+    @allure.story("测试venus-messager msg list-blocked查看消息是否正常")
+    @pytest.mark.run(order=2)
+    def test_venus_messager_msg_list_blocked(self):
+        try:
+            messager_msg_list_blocked = os.popen("/root/venus-messager msg list-blocked").readlines()
+            print ("messager msg list执行结果为：",messager_msg_list_blocked)
+            a=1
+        except Exception as e:
+            print("命令报错,错误信息为：",e)
+            a=0
+        assert a==1,"messager msg list-blocked 执行报错"
+    @allure.story("测试venus-messager msg search搜索指定消息是否正常")
+    @pytest.mark.run(order=3)
+    def test_venus_messager_msg_search_id(self):
+        msg_id=function.venus_messager_id_get('list')
+        try:
+            messager_msg_search_id_info = os.popen(f"/root/venus-messager msg search --id {msg_id}").readlines()
+            print ("命令执行结果为：",messager_msg_search_id_info)
+            if 'record not found' in messager_msg_search_id_info[0]:
+                print ("查询消息失败")
+                a=0
+            else:
+                a=1
+        except Exception as e:
+            print ("msg search 信息报错，报错信息为：",e)
+            a=0
+        assert a==1,"venus-messager msg search --id测试失败"
+@allure.epic("venus-messager测试")
+@allure.feature("venus-messager address功能模块测试")
+class Test_venus_messager_address():
+    @allure.story("测试venus-messager address list查看地址信息是否正常")
+    @pytest.mark.run(order=1)
+    def test_venus_messager_address_list(self):
+        try:
+            messager_address_list = os.popen("/root/venus-messager address list").readlines()
+            print ("messager address list执行结果为：",messager_address_list)
+            a=1
+        except Exception as e:
+            print("命令报错,错误信息为：",e)
+            a=0
+        assert a==1,"messager address list 执行报错"
+    @allure.story("测试venus-messager address search搜索指定地址信息是否正常")
+    @pytest.mark.run(order=2)
+    def test_venus_messager_address_search(self):
+        global address_addr
+        address_addr=function.venus_messager_address_get()
+        try:
+            messager_address_search = os.popen(f"/root/venus-messager address search {address_addr}").readlines()
+            print ("命令执行结果为：",messager_address_search)
+            if 'invalid address checksum' in messager_address_search[0]:
+                print ("搜索地址失败")
+                a=0
+            else:
+                a=1
+        except Exception as e:
+            print ("address search 信息报错，报错信息为：",e)
+            a=0
+        assert a==1,"venus-messager address search 测试失败"
+    @allure.story("测试venus-messager address forbidden禁用地址是否正常")
+    @pytest.mark.run(order=3)
+    def test_venus_messager_address_forbidden(self):
+        global address_addr
+        try:
+            messager_address_forbidden = os.popen(f"/root/venus-messager address forbidden {address_addr}").readlines()
+            print("命令执行结果为：", messager_address_forbidden)
+            address_status=function.venus_messager_address_state(f'{address_addr}')
+            if address_status=='4,' and 'forbidden address success!' == messager_address_forbidden[0]:
+                a = 1
+            else:
+                a = 0
+        except Exception as e:
+            print("address forbidden 信息报错，报错信息为：", e)
+            a = 0
+        assert a == 1, "venus-messager address forbidden 测试失败"
+    @allure.story("测试venus-messager address active 激活地址是否正常")
+    @pytest.mark.run(order=4)
+    def test_venus_messager_address_active(self):
+        global address_addr
+        try:
+            messager_address_active = os.popen(f"/root/venus-messager address active {address_addr}").readlines()
+            print("命令执行结果为：", messager_address_active)
+            address_status = function.venus_messager_address_state(f'{address_addr}')
+            if address_status == '1,' and 'active address success!'==messager_address_active[0]:
+                a = 1
+            else:
+                a = 0
+        except Exception as e:
+            print("address forbidden 信息报错，报错信息为：", e)
+            a = 0
+        assert a == 1, "venus-messager address active 测试失败"
+    @allure.story("测试venus-messager address del 删除地址是否正常")
+    @pytest.mark.run(order=5)
+    def test_venus_messager_address_del(self):
+        global address_addr
+        try:
+            messager_address_del = os.popen(f"/root/venus-messager address del {address_addr}").readlines()
+            print("命令执行结果为：", messager_address_del)
+            #获取删除地址状态
+            address_status = function.venus_messager_address_state(f'{address_addr}')
+            #如果地址状态为0，则说明已经无法搜索到该地址，删除正常
+            if address_status == 0:
+                a = 1
+            else:
+                a = 0
+        except Exception as e:
+            print("address forbidden 信息报错，报错信息为：", e)
+            a = 0
+        assert a == 1, "venus-messager address del 测试失败"
 if __name__ == '__main__':
-    global user_name
-    global user_rm
-    global token_rm
+    #定义全局变量address_addr，用于forbidden、active、del测试
+    global address_addr
     pytest.main()
